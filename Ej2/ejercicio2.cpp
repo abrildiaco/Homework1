d92eec679e06df8ce2997ff2713c065c9c93cf07
Abril Diaco <adiaco@udesa.edu.ar>
2025-03-14 13:28:51 -0300

    inicio ej2

---
+++ Ej2/ejercicio2.cpp
@@ -0,0 +1,24 @@
+//Cada evento recibe una etiqueta que indica su nivel de importancia o 
+//gravedad. Las etiquetas más comunes son: DEBUG, INFO, WARNING, ERROR y 
+//CRITICAL.
+
+//a. En este ejercicio, se pide crear un sistema log que permite agregar entradas a un 
+//archivo mediante el llamado a una función logMessage definida en pseudo código de 
+//la siguiente manera: 
+// 
+//void logMessage(String mensaje, Integer/Otro NivelSeveridad) 
+// 
+//Donde NivelSeveridad corresponderá con unas de las leyendas previamente 
+//mencionadas. El formato esperado en una línea del archivo de log es el siguiente: 
+//Homework #1  
+//I102 – Paradigmas de Programación 
+//[ERROR] <Mensaje> 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string respuesta; 
+    cout<<"¿Desea registrar un evento? [si/no]: "<<;
+    cin >> respuesta;
+}
