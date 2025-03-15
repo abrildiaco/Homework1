40e4facdfec159d2d712a1948f67611c8b647eca
Abril Diaco <adiaco@udesa.edu.ar>
2025-03-15 12:10:03 -0300

    final punto a. inicio pubto b

--- Ej2/ejercicio2.cpp
+++ Ej2/ejercicio2.cpp
@@ -1,49 +1,108 @@
-//Cada evento recibe una etiqueta que indica su nivel de importancia o 
-//gravedad. Las etiquetas más comunes son: DEBUG, INFO, WARNING, ERROR y 
-//CRITICAL.
+ 
+//Todos los nuevos mensajes deben ser invocados con logMessage. 
 
-//a. En este ejercicio, se pide crear un sistema log que permite agregar entradas a un 
-//archivo mediante el llamado a una función logMessage definida en pseudo código de 
-//la siguiente manera: 
-// 
-//void logMessage(String mensaje, Integer/Otro NivelSeveridad) 
-// 
-//Donde NivelSeveridad corresponderá con unas de las leyendas previamente 
-//mencionadas. El formato esperado en una línea del archivo de log es el siguiente: 
-//Homework #1  
-//I102 – Paradigmas de Programación 
-//[ERROR] <Mensaje> 
+//Se requiere la posibilidad de registrar errores, indicando el mensaje de error, el 
+//archivo y la línea de código donde sucedió este error, es decir: 
+//logMessage(String Mensage_de_Error, String Archivo, Int Línea_de_Código) 
+
+//Se requiere la posibilidad de registrar un mensaje de “Acceso de Usuario” a la 
+//aplicación. Este mensaje debe tener una leyenda nueva: [SECURITY]. La misma 
+//debe ser ingresada de la siguiente manera: 
+//logMessage(String Mensaje_De_Acceso, String Nombre_de_Usuario) 
+//Los mensajes de acceso pueden ser: Access Granted, Access Denied, etc. 
+
+//Se requiere un código que pruebe que el sistema verifica la funcionalidad 
+//requerida y que además demuestre que puede capturar un error en runtime, 
+//crear una entrada en el log y después detener la ejecución del programa y salir 
+//del mismo con un código de error (return 1).  
+
 
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
+enum class Etiquetas {DEBUG, INFO, WARNING, ERROR, CRITICAL, SECURITY};
 
-enum class Etiquetas {DEBUG, INFO, WARNING, ERROR, CRITICAL};
-string etiqueta_a_string(Etiquetas nivel) {
-    switch (nivel) {
+string etiqueta_a_string(Etiquetas severidad) {
+    switch (severidad) {
         case Etiquetas::DEBUG: return "DEBUG";
         case Etiquetas::INFO: return "INFO";
         case Etiquetas::WARNING: return "WARNING";
         case Etiquetas::ERROR: return "ERROR";
         case Etiquetas::CRITICAL: return "CRITICAL";
-        default: return "DESCONOCIDO";
+        case Etiquetas::SECURITY: return "SECURITY";
+        default: return "NO EXISTE LA ETIQUETA";
     }
 }
 
+void logMessage(string mensaje, Etiquetas NivelSeveridad){
+    
+    ofstream outFile("archivo_log.txt", ios::app);
+    
+    if (outFile.is_open()) {
+     outFile <<"["<<etiqueta_a_string(NivelSeveridad)<<"]" << "<" << mensaje <<">\n";
+     outFile.close();
+     cout << "Mensaje logeado\n";
+    
+    } else
+        cerr << "Error abriendo el archivo!\n";
+    return;
+}
+
+void logMessage(string Mensage_de_Error, string Archivo, int Línea_de_Código){
+    ofstream outFile("archivo_log.txt", ios::app);
+    
+    if (outFile.is_open()) {
+     outFile <<"[line:"<<Línea_de_Código<<"] " <<"["<<Archivo<<"]" << "<" <<  Línea_de_Código <<">\n";
+     outFile.close();
+     cout << "Mensaje logeado\n";
+    
+    } else
+        cerr << "Error abriendo el archivo!\n";
+    return;
+}
+
+void logMessage(string Mensaje_De_Acceso, string Nombre_de_Usuario, Etiquetas NivelSeveridad = Etiquetas::SECURITY){
+    ofstream outFile("archivo_log.txt", ios::app);
+    
+    if (outFile.is_open()) {
+     outFile <<"["<<etiqueta_a_string(NivelSeveridad)<<"]" <<"["<<Nombre_de_Usuario<<"]" << "<" << Mensaje_De_Acceso <<">\n";
+     outFile.close();
+     cout << "Mensaje logeado\n";
+    
+    } else
+        cerr << "Error abriendo el archivo!\n";
+    return;
+}
+
 int main(){
+
+    string respuesta;
+    cout<<"¿Desea loguear un evento? [si/no]"<<endl;
+    cin>>respuesta;
+
+    while (respuesta == "si"){
+        
+        string mensaje; 
+        cout <<"Ingrese el mensaje: "<<endl;
+        cin.ignore();
+        getline(cin, mensaje);
+        
+        int severidad;
+        cout<<"¿Cual es el nivel de severidad de su evento?\nIngrese el número corespondiente:\n0-DEBUG\n1-INFO\n2-WARNING\n3-ERROR\n4-CRITICAL"<<endl;
+        cin>> severidad;
+        
+        Etiquetas NivelSeveridad = static_cast<Etiquetas>(severidad);
+
+        logMessage(mensaje, NivelSeveridad);
+        
+        cout<<"¿Desea loguear otro evento? [si/no]"<<endl;
+        cin>>respuesta;
+    }
     
-    string respuesta; 
-    cout <<"Ingrese el mensaje: "<<endl;
-    getline(cin, respuesta);
+    if (respuesta == "no"){cout<< "programa terminado"<<endl; return 0;}
+    else if (respuesta != "si"){cout<<"Respuesta desconocida"<<endl; return 1;}
     
-    int severidad;
-    cout<<"¿Cual es el nivel de severidad de su evento?\nIngrese el número corespondiente:\n0-DEBUG\n1-INFO\n2-WARNING\n3-ERROR\n4-CRITICAL"<<endl;
-    cin>> severidad;
-    
-    Etiquetas NivelSeveridad = static_cast<Etiquetas>(severidad);
-    cout << "Mensaje: " << respuesta << endl;
-    cout << etiqueta_a_string(NivelSeveridad);
     return 0;
-    
 }
